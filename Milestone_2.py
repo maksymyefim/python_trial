25da06d33cdaa16f2d74bad3ea86138e019727d2
myefimen <max.efimenko@hotmail.com>
2023-07-07 23:49:31 +0200

    Test

--- Milestone_2.py
+++ Milestone_2.py
@@ -1,1 +1,52 @@
-testing new commit
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+          'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"This is {self.rank} of {self.suit}"
+
+test = Card(suits[0], ranks[0])
+
+print(test)
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = " "
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__()
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+test_deck = Deck()
+print(test_deck)
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.values = 0
+        self.ace = 0
+
+    def add_card(self, card):
+
+    def adjust_for_ace(self):
