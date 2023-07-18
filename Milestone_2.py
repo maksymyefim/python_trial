8674d035ebdb75e0a754ab45cc124cb7c37e0dad
myefimen <max.efimenko@hotmail.com>
2023-07-18 12:29:32 +0200

    Step 10

--- Milestone_2.py
+++ Milestone_2.py
@@ -55,11 +55,72 @@ class Hand:
         else:
             self.aces -= 1
 
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+    def win_bet(self):
+        self.total += self.bet
+    def lose_bet(self):
+        self.total -= self.bet
 
-test_deck = Deck()
-test_deck.shuffle()
-test_player = Hand()
-test_player.add_card(test_deck.deal())
-test_player.add_card(test_deck.deal())
-test_player.values
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet += int(input("Make your bet"))
+        except ValueError:
+            print("Bet must be an integer")
+        else:
+            if chips.bet>chips.total:
+                print("Your bet can't exceed",chips.total)
+            else:
+                break
 
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
+
+def hit_or_stand(deck, hand):
+    global playing
+    while True:
+        x = input("Would you like to hit or stand? Type 'h' or 's'")
+        if x[0].lower() == "h":
+            hit(deck, hand)
+        elif x[0] == "s":
+            print("Player stands. Dealer is playing")
+            playing = False
+        else:
+            print("Sorry, please try again")
+            continue
+        break
+
+def show_some(player, dealer):
+    print("\nDealer's Hand:")
+    print("<card's hidden")
+    print('',dealer.cards[1])
+    print("\nPlayer's Hand: ", *player.card, sep='\n ')
+
+def show_all(player, dealer):
+    print("\nDealer's Hand: ", *dealer.card, sep='\n ')
+    print("Dealer's score: ", dealer.value)
+    print("\nPlayer's Hand: ", *player.card, sep='\n ')
+    print("Player's score: ", player.value)
+
+def player_busts(player,dealer,chips):
+    print("Player busts!")
+    chips.lose_bet()
+
+def player_wins(player, dealer, chips):
+    print("Player wins!")
+    chips.win_bet()
+
+def dealer_busts(player, dealer, chips):
+    print("Dealer busts!")
+    chips.lose_bet()
+
+def dealer_wins(player, dealer, chips):
+    print("Dealer wins!")
+    chips.win_bet()
+
+def push(player, dealer):
+    print("It's push!")
